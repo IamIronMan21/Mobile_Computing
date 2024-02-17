64a2dcda8d15c6239d543a1a1a0557ec56a4933b
Emdadul <116040669+IamIronMan21@users.noreply.github.com>
2024-02-17 11:11:51 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.registerandlogin_haquee1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button; // Button for registration
+    private Button button2; // Button for login
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_register);
+        button2 = findViewById(R.id.btn_login);
+
+        // set up click listener for registration button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+        // set up click listener for login button
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
